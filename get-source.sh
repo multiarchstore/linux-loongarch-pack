4fcc9c49715a534b739367e212c30f544b17c3cd
Elysia <39023210+elysia-best@users.noreply.github.com>
2025-06-30 23:35:13 +0800

    feat(build): update KCONFIG_CONFIG path and add new config file

--- get-source.sh
+++ get-source.sh
@@ -10,7 +10,7 @@ rm linux.tar.gz
 mv linux-f44f34a3104a2725300dc5a8237c111e524381e6 linux
 
 # Download kernel config
-curl -fsSL -o aosc-os-abbs.tar.gz $KERNEL_CONFIG_URL
-tar -xzf aosc-os-abbs.tar.gz
-rm aosc-os-abbs.tar.gz
-mv aosc-os-abbs-36167f1f320c7203d57e222ce75aaff9bd127a45 aosc-os-abbs
+# curl -fsSL -o aosc-os-abbs.tar.gz $KERNEL_CONFIG_URL
+# tar -xzf aosc-os-abbs.tar.gz
+# rm aosc-os-abbs.tar.gz
+# mv aosc-os-abbs-36167f1f320c7203d57e222ce75aaff9bd127a45 aosc-os-abbs
