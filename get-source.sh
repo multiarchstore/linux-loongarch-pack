1a8475575bc6780a6fea0ac0160a530851cbb87b
Elysia <39023210+elysia-best@users.noreply.github.com>
2025-08-08 18:00:53 +0800

    bash refactor(get-source.sh): switch to using commit SHA for kernel source URL

--- get-source.sh
+++ get-source.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-KERNEL_SOURCE_URL="https://github.com/AOSC-Tracking/linux/archive/a546f1e85ea87f21abe49b2f961ca56aedb5f995.tar.gz"
+KERNEL_COMMIT_SHA="a546f1e85ea87f21abe49b2f961ca56aedb5f995"
+
+KERNEL_SOURCE_URL="https://github.com/AOSC-Tracking/linux/archive/$KERNEL_COMMIT_SHA.tar.gz"
 # KERNEL_CONFIG_URL="https://github.com/AOSC-Dev/aosc-os-abbs/archive/36167f1f320c7203d57e222ce75aaff9bd127a45.tar.gz"
 
 # Download kernel source
 curl -fsSL -o linux.tar.gz $KERNEL_SOURCE_URL
 tar -xzf linux.tar.gz
 rm linux.tar.gz
-mv linux-f44f34a3104a2725300dc5a8237c111e524381e6 linux
+mv "linux-$KERNEL_COMMIT_SHA" linux
 
 # Download kernel config
 # Maintainer should update the kernel config from https://github.com/AOSC-Dev/aosc-os-abbs
