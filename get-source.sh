11a0a82855de3d8f90bfabd8b271b1b1ea9735f8
Elysia <39023210+elysia-best@users.noreply.github.com>
2025-08-08 17:58:05 +0800

    chore: 6.14.11

--- get-source.sh
+++ get-source.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-KERNEL_SOURCE_URL="https://github.com/AOSC-Tracking/linux/archive/f44f34a3104a2725300dc5a8237c111e524381e6.tar.gz"
-KERNEL_CONFIG_URL="https://github.com/AOSC-Dev/aosc-os-abbs/archive/36167f1f320c7203d57e222ce75aaff9bd127a45.tar.gz"
+KERNEL_SOURCE_URL="https://github.com/AOSC-Tracking/linux/archive/a546f1e85ea87f21abe49b2f961ca56aedb5f995.tar.gz"
+# KERNEL_CONFIG_URL="https://github.com/AOSC-Dev/aosc-os-abbs/archive/36167f1f320c7203d57e222ce75aaff9bd127a45.tar.gz"
 
 # Download kernel source
 curl -fsSL -o linux.tar.gz $KERNEL_SOURCE_URL
@@ -10,7 +10,4 @@ rm linux.tar.gz
 mv linux-f44f34a3104a2725300dc5a8237c111e524381e6 linux
 
 # Download kernel config
-# curl -fsSL -o aosc-os-abbs.tar.gz $KERNEL_CONFIG_URL
-# tar -xzf aosc-os-abbs.tar.gz
-# rm aosc-os-abbs.tar.gz
-# mv aosc-os-abbs-36167f1f320c7203d57e222ce75aaff9bd127a45 aosc-os-abbs
+# Maintainer should update the kernel config from https://github.com/AOSC-Dev/aosc-os-abbs
